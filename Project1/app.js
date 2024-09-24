5a1b8029fd6b7b5f010c5dc3fecb2dfe7d7a910d
XiaoLiang0277 <94008970+XiaoLiang0277@users.noreply.github.com>
2024-09-25 02:49:15 +0800

    Add files via upload

---
+++ Project1/app.js
@@ -0,0 +1,22 @@
+
+
+function playtext() {
+    var text = document.getElementById("textplace");
+    text.style.display = "block";
+  }
+
+
+
+function showcatchy() {
+    document.getElementById('catchy')
+        .style.display = "block";
+    document.getElementById('catchyID')
+        .style.display = "none";
+}
+
+function showspider() {
+  document.getElementById('spiders')
+      .style.display = "block";
+  document.getElementById('spidersID')
+      .style.display = "none";
+}
